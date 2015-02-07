0be58ab019a38e8ec104c783f341fb081478f7e8
Jimmy Song <jaejoon@gmail.com>
2015-02-07 09:22:22 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+++Go Bitcoin Vanity Address Generator
+
+This project is a vanity address generator written in go
+
+++Install
+
+ $ go get github.com/jimmysong/govanity
+ $ govanity -prefix=123
+ Searching for prefix "123"
+
+++Donations
+
+If you found this helpful please donate to 1PBF63j1RiEV54y1EeZqskf9NN28muXDZn
+
+![bitcoin:1PBF63j1RiEV54y1EeZqskf9NN28muXDZn][Donation Image]
+
+[Donation Image]: https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=bitcoin:1PBF63j1RiEV54y1EeZqskf9NN28muXDZn?&label=Armory+Donation
---
+++ generate.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"crypto/rand"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcutil"
+)
+
+func main() {
+	beginTime := time.Now()
+
+	var prefix string
+	flag.StringVar(&prefix, "prefix", "123", "prefix you want for your vanity address")
+	flag.Parse()
+
+	fmt.Printf("Searching for prefix \"%s\"\n", prefix)
+
+	s256 := btcec.S256()
+	net := &chaincfg.MainNetParams
+
+	seed := make([]byte, 32)
+	_, err := rand.Read(seed)
+	if err != nil {
+		fmt.Printf("failed to read seed data: %s\n", err)
+		os.Exit(1)
+	}
+
+	pubkey := btcec.PublicKey{}
+	pubkey.Curve = s256
+
+	for {
+		pubkey.X, pubkey.Y = s256.ScalarBaseMult(seed)
+
+		addr1, err := btcutil.NewAddressPubKey(pubkey.SerializeCompressed(), net)
+
+		if err != nil {
+			fmt.Printf("failed to get pubkey: %s\n", err)
+			os.Exit(1)
+		}
+
+		addr2, err := btcutil.NewAddressPubKey(pubkey.SerializeUncompressed(), net)
+
+		if err != nil {
+			fmt.Printf("failed to get pubkey: %s\n", err)
+			os.Exit(1)
+		}
+
+		str1 := addr1.EncodeAddress()
+		str2 := addr2.EncodeAddress()
+
+		if strings.HasPrefix(str1, prefix) {
+			privkey, _ := btcec.PrivKeyFromBytes(s256, seed)
+			wif, err := btcutil.NewWIF(privkey, net, true)
+			if err != nil {
+				fmt.Printf("failed to get wif: %s\n", err)
+				os.Exit(1)
+			}
+			fmt.Printf("\nElapsed: %s\naddr: %s\nwif: %s\n", time.Since(beginTime), str1, wif.String())
+		}
+
+		if strings.HasPrefix(str2, prefix) {
+			privkey, _ := btcec.PrivKeyFromBytes(s256, seed)
+			wif, err := btcutil.NewWIF(privkey, net, false)
+			if err != nil {
+				fmt.Printf("failed to get wif: %s\n", err)
+				os.Exit(1)
+			}
+			fmt.Printf("\nElapsed: %s\naddr: %s\nwif: %s\n", time.Since(beginTime), str2, wif.String())
+		}
+
+		seed[0]--
+		for i := 0; seed[i] == 0; i++ {
+			seed[i+1]--
+		}
+	}
+}
